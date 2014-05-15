b2bd98038508aaf5bf067c4239bd2b93e14deebf
David Annopolsky <david.annopolsky@poptent.com>
2014-05-15 14:07:41 -0400

    added package and smart

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "Bluebird is a fully featured promise library with focus on innovative features and performance."
+});
+
+Package.on_use(function (api) {
+    
+    api.export('Promise');
+
+    api.add_files('lib/bluebird/js/browser/bluebird.js', ['client']);
+});
