9312c38e96118f857558560da61dfaf80483ba2e
Nathan Perry <nateperry333@gmail.com>
2015-08-18 15:16:30 -0400

    Add git url
    Adds Git URL so Atmosphere and other sites will pull README.md and link to this repo.

--- package.js
+++ package.js
@@ -1,5 +1,6 @@
 Package.describe({
-  summary: "Bluebird is a fully featured promise library with focus on innovative features and performance."
+  summary: "Bluebird is a fully featured promise library with focus on innovative features and performance.",
+  git: 'https://github.com/davidchase/bluebird-meteor'
 });
 
 Package.on_use(function (api) {
